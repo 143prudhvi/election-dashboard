17161d4c0ad423dcae05fa75079ec89bc6e026f0
143prudhvi <prudhvinath1998@gmail.com>
2022-10-05 08:48:37 +0530

    Initial Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React,{ useEffect, useRef } from 'react';
 import './App.css';
+import {geoMercator, geoPath, select} from 'd3';
+import {feature} from 'topojson-client';
+import useResizeObserver from './useResizeObserver';
+import data from './Himachal Pradesh.json';
 
 function App() {
+  const svgRef = useRef();
+  const wrapperRef = useRef();
+  const dimensions = useResizeObserver(wrapperRef);
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    // const {width,height} = dimensions || wrapperRef.current.getBoundingClientRect();
+    
+    const assemblies = feature(data,data.objects['Himachal Pradesh'])
+
+    const projection = geoMercator().fitSize([width-50, height-50],assemblies);
+    const pathGenerator = geoPath().projection(projection);
+
+    svg.selectAll('.assembly')
+      .data(assemblies.features)
+      .join('path')
+      .attr('class','assembly')
+      .attr('stroke', "white")
+      .attr('stroke-width' , '0.5px')
+      .attr('fill','#0066A4')
+      .attr('d', assembly => pathGenerator(assembly));
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={wrapperRef}>
+      <svg width={width-16} height={height-16} ref={svgRef}></svg>
     </div>
   );
 }
